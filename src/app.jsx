a1d1ce4261409a66f6ad2001f524af180cca8b4d
ejjang <cuttlefish77777@gmail.com>
2022-08-16 17:13:08 +0900

    router 설치 및 적용

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './app.css';
 import styled from 'styled-components';
 import Login from './components/login/login';
+import Home from './components/home/home';
 
 function App({ authService }) {
   return (
-    <AppWrapper>
-      <Login authService={authService} />
-    </AppWrapper>
+    <BrowserRouter>
+      <AppWrapper>
+        <Routes>
+          <Route path="/" element={<Login authService={authService} />} />
+          <Route path="/home" element={<Home authService={authService} />} />
+        </Routes>
+      </AppWrapper>
+    </BrowserRouter>
   );
 }
 
 const AppWrapper = styled.div`
   width: 100%;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #c3bebe;
 `;
 
 export default App;
